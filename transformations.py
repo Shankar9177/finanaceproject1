4284905c5515ff524b6281cab947b836e62c3422
Shankar Katta <shankarkatta9505@gmail.com>
2025-11-09 13:51:06 +0530

    commit to local to github

--- transformations.py
+++ transformations.py
@@ -1,3 +1,9 @@
 # transformations in this file
 
 # transformations.py
+ # This module contains various data transformation functions
+def normalize_data(data):
+    """Normalize the input data to a range of [0, 1]."""
+    min_val = min(data)
+    max_val = max(data)
+    return [(x - min_val) / (max_val - min_val) for x in data]  
